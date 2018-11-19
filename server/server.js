d75559242c83680a3cbf931dfb837342cf206e6b
gaomeiting <1959151877@qq.com>
2018-11-19 15:54:26 +0800

    server 用户接口制作

--- server/server.js
+++ server/server.js
@@ -1,39 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose')
-const DB_URL = 'mongodb://127.0.0.1:27017/chatting'
-mongoose.connect(DB_URL)
-mongoose.connection.on('connected', function () {
-    console.log('mongo connect success')
-})
-const User = mongoose.model('user', new mongoose.Schema({
-    user: { type: String, require: true},
-    age: { type: Number, require: true}
-}))
-User.create({
-    user: '李四',
-    age: '16'
-}, function (err, doc) {
-    if(!err) {
-        console.log(doc);
-    }
-    else {
-        console.log(err)
-    }
-    
-})
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
-app.get('/', function (req, res) {
-    res.send('hello world')
-    
+const userRouter = require('./user')
+
+const app = express()
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use('/user',userRouter)
+app.listen(9093,function(){
+	console.log('Node app start at port 9093')
 })
-app.get('/data', function (req, res) {
-    res.json({
-        name: '张三',
-        age: '18'
-    })
-    
-})
-app.listen(9093, function() {
-    console.log('node app start port 9093')
-})
+
+
+
